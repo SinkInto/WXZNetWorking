6322877b7ba94e97b04b23ae4dceeadc4533d301
liufeng <liufeng@meiyebang.com>
2016-05-02 18:09:58 +0800

    新建工程

---
+++ WXZNetWorking/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WXZNetWorking
+//
+//  Created by 王向召 on 16/5/2.
+//  Copyright © 2016年 王向召. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WXZNetWorkingTests/WXZNetWorkingTests.m
@@ -0,0 +1,39 @@
+//
+//  WXZNetWorkingTests.m
+//  WXZNetWorkingTests
+//
+//  Created by 王向召 on 16/5/2.
+//  Copyright © 2016年 王向召. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface WXZNetWorkingTests : XCTestCase
+
+@end
+
+@implementation WXZNetWorkingTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
